6da0f4111fa6224b02799b1ebc5204dd0e224b80
Hynek Fišera <53520774+hynekfisera@users.noreply.github.com>
2021-10-11 13:55:54 +0200

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,12 +5,12 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=2" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=2" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=2" />
-          <link rel="manifest" href="/site.webmanifest?v=2" />
-          <link rel="mask-icon" href="/safari-pinned-tab.svg?v=2" color="#246dff" />
-          <link rel="shortcut icon" href="/favicon.ico?v=2" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=3" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=3" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=3" />
+          <link rel="manifest" href="/site.webmanifest?v=3" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg?v=3" color="#246dff" />
+          <link rel="shortcut icon" href="/favicon.ico?v=3" />
           <meta name="msapplication-TileColor" content="#246dff" />
           <meta name="theme-color" content="#ffffff" />
         </Head>
